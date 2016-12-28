4c1abfb939486e25936bf7cec50926bea74f8ac2
Gerald Carreon <geraldcarreon24@gmail.com>
2016-12-28 22:03:38 +0800

    Added feature for uploading single or multiple files

--- PetSure/App/Home/ViewModels/homeModel.js
+++ PetSure/App/Home/ViewModels/homeModel.js
@@ -8,9 +8,33 @@
         $scope.selectedPet = "";
         $scope.getPets();
 
+        var dropzone = document.getElementById('dropzone');
+
+        dropzone.ondrop = function (e) {
+            e.preventDefault();
+            this.className = 'dropzone';
+            console.log(e.dataTransfer.files);
+            $scope.appendFiles(e.dataTransfer.files);
+        };
+
+        attachment.onchange = function (e) {
+            console.log(attachment.files);
+            $scope.appendFiles(attachment.files);
+        }
     }
     
+    $scope.appendFiles = function (file) {
+        $('#div-files p').remove();
+        for (var x = 0, y = file.length; x < y; x++) {
+            $('#div-files').append('<p>Filename: ' + file[x].name + '</p>');
+            fileArr[x] = file[x].name;
+        }
+    }
+
     $scope.getPets = function () {
+        fileArr = {};
+        $('#div-files p').remove();
+
         $.get('api/pet', function (data) {
             console.log(data);
             $scope.pets = data;
